ae2a4a3f51e8880f8f26712544a961f1014fe1de
Somacode <silviofrancoxa8@gmail.com>
2021-05-31 00:41:07 -0500

    added method to complete the url of the avatar image

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,7 +9,7 @@ export default class User {
     @JsonProperty("discriminator", String, true)
     discriminator?: string = undefined
     @JsonProperty("avatar", String, true)
-    avatar?: string = undefined
+    _avatar?: string = undefined
     @JsonProperty("bot", Boolean, true)
     bot?: boolean = undefined
     @JsonProperty("mfa_enabled", Boolean, true)
@@ -22,4 +22,11 @@ export default class User {
     flags?: number = undefined
     @JsonProperty("premium", Boolean, true)
     premium?: boolean = undefined
+
+    get avatar(): string {
+        if (this.avatar.startsWith("a_")) {
+            return "https://cdn.discordapp.com/avatars/" + this.id + "/" + this.avatar + ".gif?size=128"
+        }
+        return "https://cdn.discordapp.com/avatars/" + this.id + "/" + this.avatar + ".png?size=128"
+    }
 }
